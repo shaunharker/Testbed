73a6e2ed479f81324ced05177b12feb0560c3276
Shaun Harker <sharker81@gmail.com>
2021-07-22 01:25:01 -0400

    starting layernorm experiments

--- src/testbed/nn/net0.py
+++ src/testbed/nn/net0.py
@@ -2,14 +2,17 @@ import torch
 from torch.nn import Module, Embedding, Linear, CrossEntropyLoss, Softmax
 import math
 from torch.cuda.amp import autocast
+from .poly import Poly
 
 class Net0(Module):
     def __init__(self,
                  num_input_classes=256,
-                 embedding_dim=8,
-                 context_length=64,
-                 num_hidden=256,
-                 num_output_classes=256):
+                 embedding_dim=32,
+                 context_length=32,
+                 num_hidden=8192,
+                 num_output_classes=256,
+                 nonlinearity='sigmoid',
+                 degree=None):
         super().__init__()
         self.num_input_classes = num_input_classes # number of classes
         self.embedding_dim = embedding_dim # dimension of token embedding
@@ -18,57 +21,29 @@ class Net0(Module):
         self.num_output_classes = num_output_classes
         self.embedding = Embedding(self.num_input_classes, self.embedding_dim)
         self.encoder = Linear(self.embedding_dim*self.context_length, self.num_hidden)
+        self.nonlinearity = nonlinearity
+        if nonlinearity == 'sigmoid':
+            self.nonlinear = torch.nn.Sigmoid()
+        if nonlinearity == 'GELU':
+            self.nonlinear = torch.nn.GELU()
+        if nonlinearity == 'sin':
+            self.nonlinear = torch.sin
+        if nonlinearity == 'poly':
+            if degree is None:
+                degree = 7
+            self.degree = degree
+            self.nonlinear = Poly(degree=self.degree)
         self.decoder = Linear(self.num_hidden, self.num_output_classes)
         self.criterion = CrossEntropyLoss(reduction='none')
         self.softmax = Softmax(dim=-1)
 
-    def double_context_and_hidden(self):
-        E = self.embedding_dim
-        H = self.num_hidden
-        L = self.context_length
-        device = self.encoder.weight.device
-        self.num_hidden *= 2
-        self.context_length *= 2
-        encoder = Linear(self.embedding_dim*self.context_length, self.num_hidden).to(device)
-        decoder = Linear(self.num_hidden, self.num_output_classes).to(device)
-        with torch.no_grad():
-            encoder.weight[:H,:L*E] = self.encoder.weight.detach()
-            encoder.weight[:H,L*E:] = 0.0
-            encoder.weight[H:,:L*E] = 0.0
-            encoder.weight[H:,L*E:] = self.encoder.weight.detach()
-            encoder.bias[:H] = self.encoder.bias.detach()
-            encoder.bias[H:] = self.encoder.bias.detach()
-            decoder.weight[:,:H] = self.decoder.weight.detach()
-            decoder.weight[:,H:] = 0.0
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def double_hidden(self):
-        """
-        Combine with another Net0 object (initialize new weights with zeros)
-        """
-        H = self.num_hidden
-        device = self.encoder.weight.device
-        self.num_hidden *= 2
-        encoder = Linear(self.embedding_dim*self.context_length, self.num_hidden).to(device)
-        decoder = Linear(self.num_hidden, self.num_output_classes).to(device)
-        with torch.no_grad():
-            encoder.weight[:H,:] = self.encoder.weight.detach()
-            encoder.weight[H:,:] = self.encoder.weight.detach()
-            encoder.bias[:H] = self.encoder.bias.detach()
-            encoder.bias[H:] = self.encoder.bias.detach()
-            decoder.weight[:,:H] = self.decoder.weight.detach()
-            decoder.weight[:,H:] = 0.0
-        self.encoder = encoder
-        self.decoder = decoder
-
     def name(self):
-        return f"Net0({self.num_input_classes},{self.embedding_dim},{self.context_length},{self.num_hidden},{self.num_output_classes})"
+        return f"Net0({self.num_input_classes},{self.embedding_dim},{self.context_length},{self.num_hidden},{self.num_output_classes},{self.nonlinearity})"
 
     def compute_energy(self):
         return 3.0*(self.embedding_dim*self.context_length*self.num_hidden + self.num_hidden*self.num_output_classes + self.num_hidden + self.num_output_classes)/1.0E12
 
-    @autocast()
+#    @autocast()
     def forward(self, batch):
         """
         example_length = batch.shape[-1]
@@ -81,7 +56,7 @@ class Net0(Module):
         y = batch[...,-1].view(-1) # y.shape == [...]
         x = x.view(-1,self.context_length*self.embedding_dim) # x.shape == [..., self.context_length*self.embedding_dim]
         x = self.encoder(x) # x.shape == [..., self.num_hidden]
-        x = torch.sigmoid(x) # x.shape == [..., self.num_hidden]
+        x = self.nonlinear(x) # x.shape == [..., self.num_hidden]
         x = self.decoder(x) # x.shape == [..., self.num_output_classes]
         loss = self.criterion(x,y)/math.log(2)
         return loss
@@ -95,7 +70,7 @@ class Net0(Module):
         x = self.embedding(X[...,-self.context_length:]) # x.shape == [B, L, K]
         x = x.view(-1,self.context_length*self.embedding_dim) # x.shape == [..., self.context_length*self.embedding_dim]
         x = self.encoder(x) # x.shape == [..., self.num_hidden]
-        x = torch.sigmoid(x) # x.shape == [..., self.num_hidden]
+        x = self.nonlinear(x) # x.shape == [..., self.num_hidden]
         x = self.decoder(x) # x.shape == [..., self.num_output_classes]
         P = self.softmax(x) # P.shape == [..., self.num_output_classes]
         return P
--- src/testbed/nn/net4.py
+++ src/testbed/nn/net4.py
@@ -1,119 +1,68 @@
 import torch
-from torch.nn import Module, Embedding, Linear, CrossEntropyLoss, Softmax
+from torch.nn import Module, Embedding, Linear, CrossEntropyLoss, Softmax, LayerNorm
 import math
-from torch.nn.functional import pad
-import numpy as np
 from torch.cuda.amp import autocast
-
-
-class MLP(Module):
-    def __init__(self, num_input, num_hidden, num_output):
-        super().__init__()
-        self.num_input = num_input
-        self.num_hidden = num_hidden
-        self.num_output = num_output
-        self.encoder = Linear(self.num_input, self.num_hidden)
-        self.decoder = Linear(self.num_hidden, self.num_output)
-
-    def size(self):
-        return sum(torch.numel(p) for p in self.parameters())
-
-    def forward(self, x):
-        return self.decoder(torch.sigmoid(self.encoder(x)))
-
 
 class Net4(Module):
     def __init__(self,
-                 Cin=256,
-                 E=16,
-                 L=64,
-                 M=64,
-                 H=64,
-                 Cout=256,
-                 context_length=64):
+                 num_input_classes=256, #50257,
+                 embedding_dim=16,
+                 context_length=64,
+                 num_hidden=256,
+                 num_output_classes=256,
+                 nonlinearity='sigmoid'):
         super().__init__()
-        self.E = E
-        self.L = L
-        self.M = M
-        self.H = H
-        self.Cin = Cin
-        self.Cout = Cout
-        self.embedding = Embedding(Cin, E)
-        # complex encoder
-        self.W = MLP(M + L*E, H, M*M)
-        self.b = MLP(M + L*E, H, M)
-        # decoder
-        self.decoder = Linear(M, Cout)
+        self.num_input_classes = num_input_classes # number of classes
+        self.embedding_dim = embedding_dim # dimension of token embedding
+        self.context_length = context_length # context window length
+        self.num_hidden = num_hidden # number of hidden neurons
+        self.num_output_classes = num_output_classes
+        self.embedding = Embedding(self.num_input_classes, self.embedding_dim)
+        self.encoder = Linear(self.embedding_dim*self.context_length, self.num_hidden, bias=False)
+        self.normalization = LayerNorm()
+        self.nonlinearity = nonlinearity
+        if nonlinearity == 'sigmoid':
+            self.nonlinear = torch.nn.Sigmoid()
+        if nonlinearity == 'GELU':
+            self.nonlinear = torch.nn.GELU()
+        self.decoder = Linear(self.num_hidden, self.num_output_classes)
         self.criterion = CrossEntropyLoss(reduction='none')
         self.softmax = Softmax(dim=-1)
 
-    def size(self):
-        return sum(torch.numel(p) for p in self.parameters())
-
     def name(self):
-        return f"Net4({self.E},{self.L},{self.M},{self.H},size={self.size()})"
+        return f"Net0({self.num_input_classes},{self.embedding_dim},{self.context_length},{self.num_hidden},{self.num_output_classes},{self.nonlinearity})"
+
+    def compute_energy(self):
+        return 3.0*(self.embedding_dim*self.context_length*self.num_hidden + self.num_hidden*self.num_output_classes + self.num_hidden + self.num_output_classes)/1.0E12
 
     @autocast()
-    def forward(self, x0):
+    def forward(self, batch):
         """
-        x : [..., seq_length]
+        example_length = batch.shape[-1]
+        assert example_length = self.context_length + 1
         """
-        prefix = x0.shape[:-1]
-        N = x0.shape[-1]
-        L = self.L
-        E = self.E
-        M = self.M
-        m = torch.zeros(prefix + (M,), device=x0.device)
-        losses = []
-        for idx in range(N):
-            offset = idx-L
-            x = x0[...,max(0,offset):idx]
-            #print(f"Line 69. x.shape {x.shape}, x0.shape {x0.shape}, offset {offset}, idx {idx}")
-            if offset < 0:
-                #print(f"Line 71. Padding by {-offset}. idx = {idx}. N = {N}.")
-                x = pad(x,(-offset, 0))
-            y = x0[...,idx]
-            #print(f"Line 74. x.shape {x.shape} prefix {prefix} L {L} E {E} L*E {L*E}")
-            x = self.embedding(x)
-            #print(f"Line 76. x.shape {x.shape}")
-            x = x.view(*prefix, L*E)
-            #print(f"Line 78. x.shape {x.shape} m.shape {m.shape}")
-            x = torch.cat([m,x],dim=-1)
-            #print(f"Line 80. x.shape {x.shape}")
-            W = self.W(x).view(*prefix,M,M)
-            b = self.b(x).view(*prefix,M)
-            #print(f"Line 83. W.shape {W.shape}, m.shape {m.shape}, b.shape {b.shape}")
-            m = torch.sigmoid(torch.matmul(W,m.view(m.shape + (1,))).view(b.shape) + b)
-            x = self.decoder(m)
-            losses.append(self.criterion(x,y)/math.log(2))
-        return torch.cat(losses)
+        example_length = batch.shape[-1]
+        assert example_length == self.context_length + 1
+        x = batch[...,:-1] # x.shape
+        x = self.embedding(x) # x.shape == [..., self.context_length, self.embedding_dim]
+        y = batch[...,-1].view(-1) # y.shape == [...]
+        x = x.view(-1,self.context_length*self.embedding_dim) # x.shape == [..., self.context_length*self.embedding_dim]
+        x = self.encoder(x) # x.shape == [..., self.num_hidden]
+        x = self.nonlinear(x) # x.shape == [..., self.num_hidden]
+        x = self.decoder(x) # x.shape == [..., self.num_output_classes]
+        loss = self.criterion(x,y)/math.log(2)
+        return loss
 
-    @torch.no_grad()
-    def probs(self, bytes_object):
+    def probs(self, X):
         """
-        Given a bytes object, read the entirety of it with this RNN,
-        and then provide the matrix of next-byte predictions as
-        an [N, 256] numpy array, so that P[idx] gives the prediction for
-        the idx+1 byte in the bytes_object (the last one not actually being there)
+        input is tensor of shape 1 x L, long.
         """
-        # We add the 0 to get it to predict the last char
-        x0 = torch.tensor(list(bytes_object),device=self.decoder.weight.device).long()
-        N = len(bytes_object)
-        L = self.L
-        E = self.E
-        M = self.M
-        m = torch.zeros(M, device=self.decoder.weight.device)
-        P = []
-        for idx in range(N+1):
-            offset = idx-L
-            x = x0[max(0,offset):idx]
-            if offset < 0:
-                x = pad(x,(-offset, 0))
-            x = self.embedding(x).view(L*E)
-            x = torch.cat([m,x],dim=-1)
-            W = self.W(x).view(M,M)
-            b = self.b(x).view(M)
-            m = torch.sigmoid(W@m + b)
-            x = self.decoder(m)
-            P.append(self.softmax(x).view(1,-1).cpu().numpy())
-        return np.concatenate(P)
+        L = self.context_length
+        K = self.embedding_dim
+        x = self.embedding(X[...,-self.context_length:]) # x.shape == [B, L, K]
+        x = x.view(-1,self.context_length*self.embedding_dim) # x.shape == [..., self.context_length*self.embedding_dim]
+        x = self.encoder(x) # x.shape == [..., self.num_hidden]
+        x = self.nonlinear(x) # x.shape == [..., self.num_hidden]
+        x = self.decoder(x) # x.shape == [..., self.num_output_classes]
+        P = self.softmax(x) # P.shape == [..., self.num_output_classes]
+        return P
