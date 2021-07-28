ac958b51254872d72704070bad4aa1fb5efa3a06
Shaun Harker <sharker81@gmail.com>
2021-07-28 14:18:10 -0400

    refactoring

--- src/testbed/trainer/trainer.py
+++ src/testbed/trainer/trainer.py
@@ -1,137 +1,107 @@
-import torch
 import torch.multiprocessing
 ctx = torch.multiprocessing.get_context("spawn")
 Process = ctx.Process
 Queue = ctx.Queue
-from ..util import decode_broken_utf8, default_device, construct_if_required
 from .worker import Worker
-from ..data import ByteDataset
+import threading
+
 
 class Trainer:
     """
     Trainer
-    """
-    def __init__(self,
-                 path_or_config):
-        self.running = False
-        self.paused = None
-        self.metrics = []
-        self.step = 0
-        self.info = {
-            "name": "Trainer",
-            "time": 0.0,
-            "energy": 0.0}
 
+    Args:
+
+      path_or_config: the path of a checkpoint or else
+                      a configuration dictionary of the form:
+
+        {"model": {"type": ModelType, "kwargs": model_kwargs},
+         "dataset": {"type": DatasetType, "kwargs": dataset_kwargs},
+         "optimizer": {"type": OptimizerType, "kwargs": optimizer_kwargs},
+         "scheduler": {"type": SchedulerType, "kwargs": scheduler_kwargs}}
+
+    """
+    def __init__(self, path_or_config=None):
+        # communication
         self.inbox = Queue()
         self.outbox = Queue()
+
+        # self.metrics is a list that stores data for the client to use.
+        self.metrics = []
+
+        # self.metrics_inbox is a communication queue used to
+        #   receive the data that is placed in the metrics list.
         self.metrics_inbox = Queue()
+
+        # self.metrics_inbox_daemon is a thread that moves the contents
+        #   of self.metrics_inbox into self.metric.
         self.metrics_inbox_daemon = threading.Thread(
             target=self._metrics_inbox_daemon,
             daemon=True)
-        self.path_or_config = path_or_config
 
+        # self.halt_metrics_inbox_daemon is an event that signals to the
+        #   thread to break from its event loop and shut down. It is
+        #   called when the trainer is garbage collected.
+        self.halt_metrics_inbox_daemon = threading.Event()
+
+        self.path_or_config = path_or_config
+        if self.path_or_config is None:
+            raise ValueError("Trainer requires either a path or a config argument.")
+        self.process = Worker(self.outbox, self.inbox, self.metrics_inbox)
+        self.process.start()
+        self.inbox.get() # blocks until process is ready
+        self.paused = True
+        self.call("boot", path_or_config=self.path_or_config)
+
+    def __del__(self):
+        self.halt_metrics_inbox_daemon.set()
 
     def _metrics_inbox_daemon(self):
-        while True:
+        while not self.halt_metrics_inbox_daemon.is_set():
             try:
-                item = self.metrics_inbox.get(False)
-                # these three things need to be updated to checkpoint properly
-                self.step = item['step']
-                self.compute_time = item['compute_time']
-                self.compute_energy = item['compute_energy']
+                item = self.metrics_inbox.get(block=True,timeout=1.0)
+                self.metrics.append(item)
             except:
-                break
-            self.metrics.append(item)
-
-    def status(self):
-        return {"running": self.running, "paused": self.paused}
+                pass
 
     def start(self):
-        if self.running == False:
-            self.process = Worker(self.outbox, self.inbox, self.metrics_inbox)
-            self.process.start()
-            self.inbox.get() # blocks until process is ready
-            self.outbox.put(self.path_or_config)
-            self.running = True
-        self.outbox.put("start")
-        self.running = True
-        self.paused = False
+        if self.paused == True:
+            self.call("start")
+            self.paused = False
 
     def pause(self):
-        """
-        Note: blocks until the remote is paused
-        """
-        if self.running == True:
-            self.outbox.put("pause")
+        if self.paused == False:
             self.paused = True
-            self.inbox.get()
-        self.update()
+            self.call("pause")
 
     def stop(self):
-        self.pause() # to get self.step and self.compute_time
-        if self.running == True:
-            self.outbox.put("stop")
-            self.running = False
-            self.paused = None
-            self.process.join()
+        self.paused = None
+        self.call("stop")
+        self.process.join()
 
-    def load(self, path=None):
-        self.stop()
-        if path is None:
-            path = "checkpoint.pt"
-        self.outbox.put("load")
-        self.outbox.put(path)
-        self.inbox.get()
-        if self.running and not was_paused:
-            self.start()
-
-    def save(self, path=None):
-        was_paused = self.paused
-        self.pause()
-        if path is None:
-            path = "checkpoint.pt"
-        self.outbox.put("save")
-        self.outbox.put(path)
-        self.inbox.get()
-        if self.running and not was_paused:
-            self.start()
+    def save(self, path="checkpoint.pt"):
+        self.call("save", path=path)
 
     def update(self, entity, settings):
-        if self.running == True:
-            self.outbox.put("update")
-            self.outbox.put(entity)
-            self.outbox.put(settings)
-            self.inbox.get() # blocks
+        self.call("update", entity=entity, setting=settings)
 
-    def get_optimizer_stats(self):
-        stats = None
-        if self.running == True:
-            self.outbox.put("get_optimizer_stats")
-            stats = self.inbox.get()
-            if self.paused == False:
-                self.outbox.put("start")
-        return stats
+    def fetch(self, entity):
+        return self.call("fetch", "entity"=entity)
 
-    def autocomplete(self, prompt="", output_length=1024):
-        was_paused = self.paused
-        self.pause()
-        L = self.example_length - 1  # CODE SMELL (refactor net0 to not compute own loss, refactor closure to reflect new case, etc.)
-        #print(L, self.example_length)
-        self.model.eval()
-        prompt = [b for b in bytes(self.dataset.random_text_snippet() + prompt, 'utf-8')][-L:]
-        completion = []
-        tail = prompt
-        for _ in range(output_length):
-            x = (torch.tensor(tail)
-                      .unsqueeze(0)
-                      .to(default_device())) # shape [1,L]
-            #print(x.shape)
-            P = self.model.probs(x).view(-1)
-            prob_dist = torch.distributions.Categorical(P)
-            c_ord = prob_dist.sample().item()
-            tail = tail[1:] + [c_ord]
-            completion += [c_ord]
-        print(decode_broken_utf8(bytes(prompt)+bytes("\n~AUTOCOMPLETE~\n",'utf-8') + bytes(completion)))
-        if self.running and not was_paused:
-            self.start()
-        return decode_broken_utf8(bytes(completion))
+    def autocomplete(self, prompt="", output_length=256, max_ctx=512):
+        return self.call("autocomplete", prompt=prompt,
+                         output_length=output_length, max_ctx=max_ctx)
+
+    def status(self):
+        return {"paused": self.paused}
+
+    def call(self, instruction, **kwargs):
+        if self.paused is None:
+            raise RuntimeError("Trainer is defunct since stop has been called.")
+        self.outbox.put({"instruction": instruction,
+                         "kwargs": kwargs})
+        result = self.inbox.get() # blocks
+        if self.paused == False:
+            self.outbox.put({"instruction": "start"})
+            self.inbox.get()
+        return result
--- src/testbed/trainer/worker.py
+++ src/testbed/trainer/worker.py
@@ -1,20 +1,15 @@
-import numpy as np
 import gc
-import torch
-from torch.optim import AdamW
-from ..optim import Sonny
-from ..data import Loader, TextDataset
 import torch.multiprocessing
 ctx = torch.multiprocessing.get_context("spawn")
 from queue import Empty
-from ..util import IgnoreKeyboardInterrupt, Stopwatch, default_device, construct_if_required
+from ..util import IgnoreKeyboardInterrupt, Stopwatch, default_device
 import time
 import json
 
 class InstructionInterrupt(Exception):
     def __init__(self, instruction):
         super().__init__()
-        instruction = instruction
+        self.instruction = instruction
 
 class Worker(ctx.Process):
     def __init__(self, inbox, outbox, metrics_outbox):
@@ -22,11 +17,13 @@ class Worker(ctx.Process):
         self.inbox = inbox
         self.outbox = outbox
         self.metrics_outbox = metrics_outbox
+        self.parent = torch.multiprocessing.parent_process()
 
         self.model = None
         self.dataset = None
         self.optimizer = None
         self.scheduler = None
+
         self.step = 0
         self.info = {
             "name": "Worker",
@@ -50,6 +47,8 @@ class Worker(ctx.Process):
         print(json.dumps(log_message))
 
     def closure(self):
+        self.minibatches *= 2
+        self.minibatch_size = self.batch_size // self.minibatches
         while True:
             try:
                 return self._closure()
@@ -123,49 +122,65 @@ class Worker(ctx.Process):
 
     def run(self):
         self.log(f"Worker.run")
-        with IgnoreKeyboardInterrupt(): # Jupyter sends these in error sometimes when cells are interrupted.
-            self.outbox.put("ready")
-            job = self.inbox.get()
-            self.model = construct_if_required(job["model"])
-            self.optimizer = construct_if_required(job["optimizer"])
-            self.dataset = construct_if_required(job["dataset"])
-            self.scheduler = construct_if_required(job["scheduler"])
-            self.info = job["profile"]
-            self.step = job["step"]
-            self.metrics = job["metrics"]
-            self.parent = torch.multiprocessing.parent_process()
-            instruction = "pause"
-            while instruction != "stop":
+        self.outbox.put("ready")
+        job = self.inbox.get()
+        instruction = job["instruction"]
+        assert instruction == "boot"
+        if "path" in job:
+            self.path = job["path"]
+            make = lambda x: x["type"](**x["kwargs"])
+            self.model = make(self.path["model"])
+            self.optimizer = make(self.path["optimizer"])
+            self.dataset = make(self.path["dataset"])
+            self.scheduler = make(self.path["scheduler"])
+        elif "config" in job:
+            self.config = job["config"]
+            self.load(self.config)
+        else:
+            raise RuntimeError("Invalid job")
+
+        # Jupyter sends KeyboardInterrupt in error sometimes
+        # when cells are interrupted, so we ignore them in our
+        # main event loop:
+        with IgnoreKeyboardInterrupt():
+            while True:
                 try:
-                    instruction = self.inbox.get(True,1.0)
-                    self.dispatch(instruction)
+                    job = self.inbox.get(True,1.0)
+                    result = self.dispatch(job["instruction"],
+                                           job["kwargs"])
+                    self.outbox.put(result)
+                    if job["instruction"] == "stop":
+                        break
                 except Empty:
                     if not self.parent.is_alive():
                         self.log("orphaned")
-                        instruction = "stop"
+                        break
         self.log("terminate")
 
-    def dispatch(self, instruction):
+    def dispatch(self, instruction, kwargs):
         self.log(f"dispatch({instruction}")
         if instruction == "start":
+            self.log("start")
             instruction = self.train()
             # ... and then case fall-through for whatever it returns.
+        if instruction == "stop":
+            self.log("stop")
+            return None
         if instruction == "update":
-            entity = self.inbox.get()
-            settings = self.inbox.get()
-            self.update(entity, settings)
-        if instruction == "get_optimizer_stats":
-            self.log("get_optimizer_stats")
-            self.outbox.put(self.optimizer.state['stats'])
+            entity = kwargs["entity"]
+            settings = kwargs["settings"]
+            return self.update(entity, settings)
+        if instruction == "fetch":
+            entity = kwargs["entity"]
+            return self.fetch(entity)
         if instruction == "save":
-            savefile = self.inbox.get()
-            self.save(savefile)
-            self.outbox.put("saved")
+            path = kwargs["path"]
+            return self.save(path)
         if instruction == "pause":
-            self.outbox.put("paused")
+            return "paused"
 
     def update(self, entity, settings):
-        self.log(f"{entity}.update({settings})")
+        self.log(f"update({entity},{settings})")
         if entity == "model":
             self.model.update(settings)
         if entity == "optimizer":
@@ -175,13 +190,25 @@ class Worker(ctx.Process):
         if entity == "scheduler":
             self.scheduler.update(settings)
 
+    def fetch(self, entity):
+        self.log(f"fetch({entity})")
+        if entity == "model":
+            return None # TODO
+        if entity == "optimizer":
+            return None # TODO
+        if entity == "dataset":
+            return None # TODO
+        if entity == "scheduler":
+            return None # TODO
+
     def load(self, path):
         self.log(f"load({path})")
         checkpoint = torch.load(path)
+        make = lambda x: x["type"](**x["kwargs"])
         self.model = checkpoint["model"]
-        self.dataset = construct_if_required(checkpoint["dataset"])
-        self.optimizer = construct_if_required(checkpoint["optimizer"])
-        self.scheduler = construct_if_required(checkpoint["scheduler"])
+        self.dataset = make(checkpoint["dataset"])
+        self.optimizer = checkpoint["optimizer"]
+        self.scheduler = checkpoint["scheduler"]
         self.step = checkpoint["step"]
         self.profile = checkpoint["profile"]
         self.metrics = checkpoint["metrics"]
