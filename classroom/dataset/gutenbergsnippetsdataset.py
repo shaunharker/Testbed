1a45e8c59e26944a4a37f0cc6eaa1b9499773073
Shaun Harker <sharker81@gmail.com>
2021-09-11 08:51:22 -0400

    mucking about

--- classroom/dataset/gutenbergsnippetsdataset.py
+++ classroom/dataset/gutenbergsnippetsdataset.py
@@ -4,22 +4,31 @@ from random import randrange
 import numpy as np
 import torch
 import os
+from .utf8 import utf8decode, utf8encode
 
 user = os.environ["USER"]
 
 
 class GutenbergSnippetsDataset:
-    def __init__(self, path=None, device='cuda'):
-        self.path = path or f"/home/{user}/data/gutenberg.1024.utf8"
+    def __init__(self, path=None, line=None, device='cuda'):
+        if path is None:
+            if line is None:
+                line = 1024
+            path = f"/home/{user}/data/gutenberg.{line}.utf8"
+        self.path = path
+        self.line = line
         self.device = device
+        self.decode = utf8decode
+        self.encode = utf8encode
         self._load()
 
     def batch(self, batch_size, example_length, offset = None):
-        sz = batch_size*1024
-        offset = offset or randrange(self.n_bytes)
+        L = self.line or 2*example_length
+        sz = batch_size*L
+        offset = offset if offset is not None else randrange(self.n_bytes)
         batch_offset = offset%(self.n_bytes//sz)
-        example_offset = (offset//(self.n_bytes//sz))%(1024-example_length)
-        return torch.tensor(self.data[sz*batch_offset:sz*batch_offset+sz].reshape(batch_size, 1024)[:,example_offset:example_offset+example_length], dtype=torch.long, device=self.device)
+        example_offset = (offset//(self.n_bytes//sz))%(L-example_length)
+        return torch.tensor(self.data[sz*batch_offset:sz*batch_offset+sz].reshape(batch_size, L)[:,example_offset:example_offset+example_length], dtype=torch.long, device=self.device)
 
     def __getstate__(self):
         state = self.__dict__.copy()
--- classroom/model/nn.py
+++ classroom/model/nn.py
@@ -314,28 +314,34 @@ class Attn(Module):
 
 
 class FastformerAttn(Module):
-    def __init__(self, d_model, d_k, d_v, n_heads, p_dropout):
+    def __init__(self, d_model, d_q, d_k, d_v, n_heads):
         super().__init__()
         self.d_model = d_model
+        self.d_q = d_q
         self.d_k = d_k
         self.d_v = d_v
         self.n_heads = n_heads
-        self.p_dropout = p_dropout
 
-        self.query_proj = Linear(d_model, d_k*n_heads)
-        self.key_proj = Linear(d_model, d_k*n_heads)
-        self.value_proj = Linear(d_model, d_v*n_heads)
-        self.mask = Mask(mode="causal")
-        self.dropout = Dropout(p_dropout)
+        self.split_heads = lambda x: x.view(x.shape[:-1]+(self.n_heads,-1)).transpose(-2,-3).contiguous()
+        self.query_transformation = Sequential(
+            Linear(d_model, d_q*n_heads),
+            Lambda(self.split_heads))
+        self.key_transformation = Sequential(
+            Linear(d_model, d_k*n_heads),
+            Lambda(self.split_heads))
+        self.value_transformation = Sequential(
+            Linear(d_model, d_v*n_heads),
+            Lambda(self.split_heads))
         self.softmax = torch.nn.Softmax(dim=-1)
-        self.linear = Linear(d_v*n_heads, d_model, bias=False)
-
+        self.alpha = Sequential(Linear(d_q, 1), torch.nn.Softmax(dim=-2))
+        self.beta = Sequential(Linear(d_k, 1), torch.nn.Softmax(dim=-2))
+        self.merge_heads = lambda x: x.transpose(-2,-3).contiguous().view(x.shape[:-3]+(n_ctx,self.d_v*self.n_heads))
     def forward(self, x):
         (n_ctx, d_model) = x.shape[-2:]
         assert d_model == self.d_model, f"{d_model} != {self.d_model}"
-        split_heads = lambda x: x.view(x.shape[:-1]+(self.n_heads,-1)).transpose(-2,-3).contiguous()
-        merge_heads = lambda x: x.transpose(-2,-3).contiguous().view(x.shape[:-3]+(n_ctx,self.d_v*self.n_heads))
+
         (Q, K, V) = map(split_heads,(self.query_proj(x),self.key_proj(x),self.value_proj(x)))
+        # Q =
         QKT = torch.matmul(Q/math.sqrt(self.d_k),K.transpose(-1,-2))
         return self.linear(merge_heads(self.dropout(self.softmax(self.mask(QKT)))@V))
 
