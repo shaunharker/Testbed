268122450b14a592b88090131917188c36c1c643
Shaun Harker <sharker81@gmail.com>
2023-07-01 01:43:09 -0400

    updates

--- scholar/model/persephone.py
+++ scholar/model/persephone.py
@@ -1,7 +1,6 @@
 import math
 import torch
-import copy
-from torch.nn import Module, Linear, LayerNorm, Embedding, ModuleList, Sigmoid
+from torch.nn import Module, Linear, Embedding, ModuleList, LayerNorm, GELU
 
 
 class Mask(Module):
@@ -46,56 +45,79 @@ class Attn(Module):
 
 
 class PersephoneLayer(Module):
-    def __init__(self, d_in, d_hidden, d_out, d_k, d_v, n_heads):
+    def __init__(self, d_model, d_k, d_v, n_heads):
         super().__init__()
-        self.d_in = d_in
-        self.d_out = d_out
+        self.d_model = d_model
         self.d_k = d_k
         self.d_v = d_v
         self.n_heads = n_heads
-        self.attn = Attn(d_in, d_hidden, d_k, d_v, n_heads, 'causal')
-        self.nonlinearity = Sigmoid()
-        self.ff = Linear(d_hidden, d_out, bias=True)
-
-        self.residual = Linear(d_in, d_out, bias=False)
-        self.residual.weight.data *= 0.0
-        rows = torch.arange(max(d_in, d_out)) % d_in
-        cols = torch.arange(max(d_in, d_out)) % d_out
-        self.residual.weight.data[rows, cols] = 1.0
-
-
+        self.attn = Attn(d_model, d_model, d_k, d_v, n_heads, 'causal')
+        self.ln1 = LayerNorm(d_model)
+        self.ff1 = Linear(d_model, 4*d_model)
+        self.nonlinearity = GELU()
+        self.ff2 = Linear(4*d_model, d_model)
+        self.ln2 = LayerNorm(d_model)
+        self.mlp = lambda x: self.ff2(self.nonlinearity(self.ff1(x)))
     def forward(self, x):
-        return self.ff(self.nonlinearity(self.attn(x))) + self.residual(x)
+        x1 = x + self.ln1(self.attn(x))
+        x2 = x1 + self.ln2(self.mlp(x1))
+        return x2
     
+# i was doing this before: look, the entire thing is just summing a layernormed contrib and it worked.
+# but... would it have been better to not force attn through mlp? recent experiments suggest this was
+# in fact critical.
+# but what about
+
+#  x1 = x + self.ln1(self.attn(x))
+#  x2 = x1 + self.ln2(self.mlp(x1)) 
+#
+#  which is NOT equivalent!
+# def forward(self, x):
+# if self.use_layernorms:
+#     return x + self.ln2(self.mlp(x + self.ln1(self.attn(x))))
+# else:
+#     return x+self.mlp(x+self.attn(x))
+
 class Persephone(Module):
     def __init__(self,
                  n_vocab_in=256,
                  n_vocab_out=256,
                  n_ctx=4096,
-                 d_embd=1024):
+                 d_model=1024):
         super().__init__()
         self.n_vocab_in = n_vocab_in
         self.n_vocab_out = n_vocab_out
         self.n_ctx = n_ctx
-        self.d_embd = d_embd
-
-        self.embedding = Embedding(n_vocab_in, d_embd)
-        self.positional_encoding = Embedding(n_ctx, d_embd)
-        self.positional_encoding.weight.data = 0.02*torch.randn(n_ctx, d_embd)
+        self.d_model = d_model
+        self.embeddings = ModuleList()
+        self.positional_encodings = ModuleList()
         self.layers = ModuleList()
         self.read_heads = ModuleList()
 
+    def add_layer(self, d_k=None, d_v=None, n_heads=None):
+        device = 'cuda'
+        embedding = Embedding(self.n_vocab_in, self.d_model).to(device)
+        positional_encoding = Embedding(self.n_ctx, self.d_model).to(device)
+        positional_encoding.weight.data *= 0.02
+        layer = PersephoneLayer(self.d_model, d_k, d_v, n_heads).to(device)
+        read_head = Linear(self.d_model, self.n_vocab_out).to(device)
+        self.embeddings.append(embedding)
+        self.positional_encodings.append(positional_encoding)
+        self.layers.append(layer)
+        self.read_heads.append(read_head)
 
-    def add_layer(self, d_in=None, d_hidden=None, d_out=None, d_k=None, d_v=None, n_heads=None):
-        device = self.embedding.weight.device # arbitrarily chosen parameter to read a 'device' copy
-        self.layers.append(PersephoneLayer(d_in, d_hidden, d_out, d_k, d_v, n_heads).to(device))
-        self.read_heads.append(Linear(d_out, self.n_vocab_out).to(device))
-
-    def forward(self, x):
-        x = self.embedding(x)
-        x = x + self.positional_encoding.weight[:x.shape[-2],:]
+    def forward(self, input_ids):
+        device = input_ids.device
+        positions = torch.arange(input_ids.size(-1), device=device)
         ys = []
-        for (layer, read_head) in zip(self.layers, self.read_heads):
+        x = None
+        for embedding, positional_encoding, layer, read_head in zip(
+            self.embeddings, self.positional_encodings, self.layers, self.read_heads
+        ):
+            if x is None:
+                x = embedding(input_ids) + positional_encoding(positions)
+            else:
+                x = x + embedding(input_ids) + positional_encoding(positions)
             x = layer(x)
             y = read_head(x)
             ys.append(y)
@@ -106,14 +128,12 @@ class Persephone(Module):
             'n_vocab_in': self.n_vocab_in,
             'n_vocab_out': self.n_vocab_out,
             'n_ctx': self.n_ctx,
-            'd_embd': self.d_embd,
         }  
-    
+
     def set_config(self, config):
         self.n_vocab_in = config.get('n_vocab_in', self.n_vocab_in)
         self.n_vocab_out = config.get('n_vocab_out', self.n_vocab_out)
         self.n_ctx = config.get('n_ctx', self.n_ctx)
-        self.d_embd = config.get('d_embd', self.d_embd)
 
     def save(self, path):
         torch.save({
@@ -125,6 +145,6 @@ class Persephone(Module):
     def load(path):
         checkpoint = torch.load(path)
         config = checkpoint.get('config', {})
-        model = Persephone(**config)  # replace with your actual model constructor
+        model = Persephone(**config)
         model.load_state_dict(checkpoint['model_state_dict'])
         return model
